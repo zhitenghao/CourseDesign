bc28e60dcb68fce7e24e650fbdc048a38b164c50
DaiRui <NVRGUP721@163.com>
2021-11-14 16:48:11 +0800

    update By DaiRui

--- communication-platform/src/main/java/com/swjt/community/controller/UploadController.java
+++ communication-platform/src/main/java/com/swjt/community/controller/UploadController.java
@@ -21,6 +21,9 @@ import org.springframework.web.multipart.MultipartFile;
 import sun.misc.BASE64Encoder;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
 
 
 @RestController
@@ -34,45 +37,55 @@ public class UploadController {
     @CrossOrigin
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
     public Result upload(@RequestParam(value = "file") MultipartFile file, HttpServletRequest request) {
-        String id=uploadServiceImpl.upload(file,request);
+        String id = uploadServiceImpl.upload(file, request);
         return Result.succ(
                 MapUtil.builder()
-                .put("msg","存储成功")
-                .put("id",id)
-                .map()
+                        .put("msg", "存储成功")
+                        .put("id", id)
+                        .map()
         );
     }
 
     @ApiOperation(value = "图片文件下载")
-    @ApiImplicitParams({ @ApiImplicitParam(name = "id", value = "图片id", required = true, paramType = "path") })
+    @ApiImplicitParams({@ApiImplicitParam(name = "id", value = "图片id", required = true, paramType = "path")})
     @CrossOrigin
     @RequestMapping(value = "/downloadPhoto/{id}", method = RequestMethod.GET)
-    public Result downloadPhoto(@PathVariable(value = "id") String id, HttpServletRequest request) {
-        byte[] bytes=uploadServiceImpl.downloadPhoto(id, request);
-        BASE64Encoder encoder=new BASE64Encoder();
-        String str="data:image/jpeg;base64,";
-        String base64Img=str+encoder.encode(bytes);
-        return Result.succ(
-                MapUtil.builder()
-                .put("imgSrc",base64Img)
-                .map()
-        );
+    public void downloadPhoto(@PathVariable(value = "id") String id, HttpServletRequest request,HttpServletResponse response) throws IOException{
+        byte[] bytes = uploadServiceImpl.downloadPhoto(id, request);
+        response.setContentType("image/jpeg;charset=UTF-8");
+        response.addHeader("Content-Length", "" + bytes.length);
+        OutputStream stream = response.getOutputStream();
+        stream.write(bytes);
+        stream.flush();
+        stream.close();
+//        BASE64Encoder encoder = new BASE64Encoder();
+//        String str = "data:image/jpeg;base64,";
+//        String base64Img = str + encoder.encode(bytes);
+//        return Result.succ(
+//                MapUtil.builder()
+//                        .put("imgSrc", base64Img)
+//                        .map()
+//        );
     }
+
+    /**
+     * 文件流形式下载视频
+     *
+     * @author Front Ng
+     * @date 2019-05-23 09:25
+     **/
 
     @ApiOperation(value = "视频文件下载")
-    @ApiImplicitParams({ @ApiImplicitParam(name = "id", value = "视频id", required = true, paramType = "path") })
+    @ApiImplicitParams({@ApiImplicitParam(name = "id", value = "视频id", required = true, paramType = "path")})
     @CrossOrigin
     @RequestMapping(value = "/downloadVideo/{id}", method = RequestMethod.GET)
-    public Result downloadVideo(@PathVariable(value = "id") String id, HttpServletRequest request) {
-        byte[] bytes=uploadServiceImpl.downloadVideo(id, request);
-        BASE64Encoder encoder=new BASE64Encoder();
-        String str="data:video/mp4;base64,";
-        String base64Img=str+encoder.encode(bytes);
-        return Result.succ(
-                MapUtil.builder()
-                .put("imgSrc",base64Img)
-                .map()
-        );
-    }
-
+    public void downloadVideo(@PathVariable String id,HttpServletResponse response,HttpServletRequest request) throws IOException {
+        byte[] bytes = uploadServiceImpl.downloadVideo(id, request);
+        response.setContentType("application/octet-stream;charset=UTF-8");
+        response.addHeader("Content-Length", "" + bytes.length);
+        OutputStream stream = response.getOutputStream();
+        stream.write(bytes);
+        stream.flush();
+        stream.close();
+        }
 }
