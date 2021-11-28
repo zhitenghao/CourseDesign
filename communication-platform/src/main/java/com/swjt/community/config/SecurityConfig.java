876115f166fb40babc2ede89c021d2b41f0a4dcc
NVRGUP-DR <NVRGUP721@163.com>
2021-11-28 22:17:57 +0800

    update by DaiRui

--- communication-platform/src/main/java/com/swjt/community/config/SecurityConfig.java
+++ communication-platform/src/main/java/com/swjt/community/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     JwtLogoutSuccessHandler jwtLogoutSuccessHandler;
-    
+
     @Autowired
     MyUsernamePasswordAuthenticationHandler myUsernamePasswordAuthenticationHandler;
 
--- communication-platform/src/main/java/com/swjt/community/security/MyUsernamePasswordAuthenticationHandler.java
+++ communication-platform/src/main/java/com/swjt/community/security/MyUsernamePasswordAuthenticationHandler.java
@@ -6,15 +6,61 @@ import org.springframework.security.authentication.AuthenticationServiceExceptio
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 @Component
-public class MyUsernamePasswordAuthenticationHandler extends UsernamePasswordAuthenticationFilter {
+public class MyUsernamePasswordAuthenticationHandler extends AbstractAuthenticationProcessingFilter {
+    public static final String SPRING_SECURITY_FORM_USERNAME_KEY = "username";
+    public static final String SPRING_SECURITY_FORM_PASSWORD_KEY = "password";
+    private String usernameParameter = SPRING_SECURITY_FORM_USERNAME_KEY;
+    private String passwordParameter = SPRING_SECURITY_FORM_PASSWORD_KEY;
     private boolean postOnly = true;
+
+    public MyUsernamePasswordAuthenticationHandler() {
+        super(new AntPathRequestMatcher("/login", "POST"));
+    }
+
+    protected String obtainPassword(HttpServletRequest request) {
+        return request.getParameter(passwordParameter).replaceAll(" ", "+");
+    }
+
+    protected String obtainUsername(HttpServletRequest request) {
+        return request.getParameter(usernameParameter);
+    }
+
+    protected void setDetails(HttpServletRequest request,
+                              UsernamePasswordAuthenticationToken authRequest) {
+        authRequest.setDetails(authenticationDetailsSource.buildDetails(request));
+    }
+
+    public void setUsernameParameter(String usernameParameter) {
+        Assert.hasText(usernameParameter, "Username parameter must not be empty or null");
+        this.usernameParameter = usernameParameter;
+    }
+
+    public void setPasswordParameter(String passwordParameter) {
+        Assert.hasText(passwordParameter, "Password parameter must not be empty or null");
+        this.passwordParameter = passwordParameter;
+    }
+
+    public void setPostOnly(boolean postOnly) {
+        this.postOnly = postOnly;
+    }
+
+    public final String getUsernameParameter() {
+        return usernameParameter;
+    }
+
+    public final String getPasswordParameter() {
+        return passwordParameter;
+    }
 
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
         if (this.postOnly && !request.getMethod().equals("POST")) {
