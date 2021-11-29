a63ea22e03e76e7d744cce735cf3b1bd4b35b295
Sykevin <80102399+Sykevin@users.noreply.github.com>
2021-11-29 16:02:40 +0800

    update

---
+++ communication-web/src/components/Articles/category.vue
@@ -0,0 +1,24 @@
+<template>
+  <articles :categoryId="categoryId"></articles>
+</template>
+
+<script>
+import Articles from '@/components/Articles/index.vue'
+
+export default {
+  name: 'category',
+  components: { Articles },
+  props:[
+    'id'
+  ],
+  data(){
+    return {
+      categoryId: '/listByCategory/'+this.id
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
